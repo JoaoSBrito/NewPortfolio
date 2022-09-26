f8ff97c94a534d3ac6998df42b5bd1e0e6c0fa47
Joao Vitor Brito <joaovbrito123@gmail.com>
2022-09-26 16:45:30 -0300

    Adding projects

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -19,9 +19,29 @@ export function Projects() {
           />
 
         <ProjectItem 
-          href="https://todo-list-ten-chi.vercel.app/"
-          src="src/assets/Pomodoro-Timer.png"
-          title="Pomodoro Timer"
+          href="https://tela-login-woad.vercel.app/"
+          src="src/assets/login.png"
+          title="Tela de Login"
+          />
+
+        <ProjectItem 
+          href="https://criacaoconta-com-validacao.vercel.app/"
+          src="src/assets/creation.png"
+          title="Tela de Criação"
+          />
+      </ProjectList>
+
+      <ProjectList>
+        <ProjectItem 
+          href="https://nlw-return-seven-fawn.vercel.app/"
+          src="src/assets/nlw-return.png"
+          title="NLW Return"
+          />
+
+        <ProjectItem 
+          href="https://nlw-beautysalon-eta.vercel.app/"
+          src="src/assets/nlw-beautysalon.png"
+          title="NLW BeautySalon"
           />
       </ProjectList>
     </Container>
