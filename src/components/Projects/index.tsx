786cf414b1dc31ec76a2cd1394db2997e6441b1c
Joao Vitor Brito <joaovbrito123@gmail.com>
2022-11-11 11:29:32 -0300

    Added CoffeeDelivery Project

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,15 +1,15 @@
-import { Container, Title } from '../../global'
+import { Container, Title } from "../../global";
 
-import TodoList from '../../assets/Todolist.png'
-import Login from '../../assets/login.png'
-import Creation from '../../assets/creation.png'
-import NlwReturn from '../../assets/nlw-return.png'
-import NlwBeauty from '../../assets/nlw-beautysalon.png'
+import TodoList from "../../assets/Todolist.png";
+import Login from "../../assets/login.png";
+import Creation from "../../assets/creation.png";
+import NlwReturn from "../../assets/nlw-return.png";
+import NlwBeauty from "../../assets/nlw-beautysalon.png";
+import CoffeeDelivery from "../../assets/CoffeeDelivery.png";
 // import pomodoTimer from 'src/assets/Pomodoro-Timer.png'
 
-
-import { ProjectList } from './Project.styles'
-import { ProjectItem } from './ProjectItem'
+import { ProjectList } from "./Project.styles";
+import { ProjectItem } from "./ProjectItem";
 
 export function Projects() {
   return (
@@ -17,38 +17,44 @@ export function Projects() {
       <Title>Projetos</Title>
 
       <ProjectList>
-        <ProjectItem 
+        <ProjectItem
           href="https://todo-list-ten-chi.vercel.app/"
           src={TodoList}
           title="TodoList"
-          />
+        />
 
-        <ProjectItem 
+        <ProjectItem
           href="https://tela-login-woad.vercel.app/"
           src={Login}
           title="Tela de Login"
-          />
+        />
 
-        <ProjectItem 
+        <ProjectItem
           href="https://criacaoconta-com-validacao.vercel.app/"
           src={Creation}
           title="Tela de Criação"
-          />
+        />
       </ProjectList>
 
       <ProjectList>
-        <ProjectItem 
+        <ProjectItem
           href="https://nlw-return-seven-fawn.vercel.app/"
           src={NlwReturn}
           title="NLW Return"
-          />
+        />
 
-        <ProjectItem 
+        <ProjectItem
           href="https://nlw-beautysalon-eta.vercel.app/"
           src={NlwBeauty}
           title="NLW BeautySalon"
-          />
+        />
+
+        <ProjectItem
+          href="https://nlw-beautysalon-eta.vercel.app/"
+          src={CoffeeDelivery}
+          title="Coffee Delivery"
+        />
       </ProjectList>
     </Container>
-  )
+  );
 }
